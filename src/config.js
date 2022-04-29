e131dc3aeda7bbf330d6aae01cc8a7389a02caea
Pablo Correa <pablocorrea523@gmail.com>
2022-04-29 00:35:31 -0400

    agrego .env

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "08698470035591491f37f82169628d2d";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=es-MX&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=es-MX`;
